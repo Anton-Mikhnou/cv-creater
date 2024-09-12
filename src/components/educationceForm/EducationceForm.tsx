b6bfa9bbe9f9c3e4bf9cdeb81821d9f9ef38902c
Anton Mikhnou <anton_mihnov03@mail.ru>
2024-09-12 14:15:13 +0300

    end the project

--- src/components/educationceForm/EducationceForm.tsx
+++ src/components/educationceForm/EducationceForm.tsx
@@ -1,6 +1,5 @@
 import style from './EducationceForm.module.scss'
 import { Educationce } from '../main/main'
-import { Value } from 'sass';
 
 interface EducationceProps {
     educationce: Educationce,
@@ -9,52 +8,39 @@ interface EducationceProps {
 
 export default function EducationceForm({educationce, setEducationce}: EducationceProps) {
 
-    const handelSubmit = (event) => {
-        event.preventDefault();
-        
-        const dataToSubmit = {
-            ...educationce
-        };
-    }
-
-    const handelInputChange = (event) => {
-        const {target}  = event;
-        const {name, value} = target;
+    const handelInputChange = (event: React.FormEvent<HTMLInputElement>): void => {
+        const {name, value} = event.currentTarget;
 
         setEducationce({
             ...educationce,
             [name]: value
         });
+    
     }
 
     return (
         <>
             <div className={style['container']}>
 
-                <form action="#" method='post'>
+                <label htmlFor="nameOfInstitution">
+                    Name of Institution:
+                    <input type="text" name='nameOfInstitution' id='nameOfInstitution' onChange={handelInputChange}/>
+                </label>
 
-                    <label htmlFor="nameOfInstitution">
-                        Name of Institution:
-                        <input type="text" name='nameOfInstitution' id='nameOfInstitution' onChange={e => setEducationce({...educationce, nameOfInstitution: e.target.value})}/>
-                    </label>
-                    
-                    <label htmlFor="titleOfStudy">
-                        Title of Study:
-                        <input type="titleOfStudy" name='titleOfStudy' id='titleOfStudy' onChange={e => setEducationce({...educationce, titleOfStudy: e.target.value})} />
-                    </label>
+                <label htmlFor="titleOfStudy">
+                    Title of Study:
+                    <input type="titleOfStudy" name='titleOfStudy' id='titleOfStudy' onChange={handelInputChange} />
+                </label>
 
-                    <label htmlFor="start">
-                        Start of studies:
-                        <input type="date" name='start' id='start' onChange={e => setEducationce({...educationce, start: e.target.value})}/>
-                    </label>
+                <label htmlFor="start">
+                    Start of studies:
+                    <input type="date" name='start' id='start' onChange={handelInputChange}/>
+                </label>
 
-                    <label htmlFor="end">
-                        End of studies:
-                        <input type="date" name='end' id='end' onChange={e => setEducationce({...educationce, end: e.target.value})} />
-                    </label>
-
-                    <button type='submit'>Add Education</button>
-                </form>
+                <label htmlFor="end">
+                    End of studies:
+                    <input type="date" name='end' id='end' onChange={handelInputChange} />
+                </label>
 
             </div>
         </>
